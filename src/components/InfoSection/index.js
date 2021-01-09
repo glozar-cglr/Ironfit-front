c2003f304b5341cef8e3aa0d03c47ad2cd5bdb3f
Alejandra <alejandrariscanevo@gmail.com>
2021-01-09 12:06:40 -0500

    Landing v.2

--- src/components/InfoSection/index.js
+++ src/components/InfoSection/index.js
@@ -2,9 +2,9 @@
 
 import React from 'react';
 import { Button } from '../ButtonElement';
-import {InfoContainer, InfoWrapper, InfoRow, Column1, Column2, TextWrapper, TopLine, Heading, Subtitle, BtnWrap,ImgWrap, Img } from './InfoElements'
+import {InfoContainer, InfoWrapper, InfoRow, Column1, Column2, TextWrapper, TopLine, Heading, Subtitle, BtnWrap, ImgWrap, Img } from './InfoElements'
 
-const InfoSection = ({lightBg, id, imgStart, topLine, lightText, headline, darkText, description, buttonLabel, img, alt, primary, dark, dark2}) => {
+const InfoSection = ({lightBg, id, imgStart, topLine, lightText, headLine, darkText, description, buttonLabel, img, alt, primary, dark, dark2}) => {
     return (
         <>
          <InfoContainer lightBg={lightBg} id={id}>
@@ -13,7 +13,7 @@ const InfoSection = ({lightBg, id, imgStart, topLine, lightText, headline, darkT
                      <Column1>
                         <TextWrapper>
                             <TopLine>{topLine}</TopLine>
-                            <Heading lightText={lightText}>{headline}</Heading>
+                            <Heading lightText={lightText}>{headLine}</Heading> 
                             <Subtitle darkText={darkText}>{description}</Subtitle>
                             <BtnWrap>
                                 <Button to="home"
