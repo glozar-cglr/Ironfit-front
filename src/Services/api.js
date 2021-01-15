18715014478044f1727e08552acc805b06e40102
Gonzalo Loza <cg.lozarojas@gmail.com>
2021-01-14 22:53:53 -0500

    heroku

--- src/Services/api.js
+++ src/Services/api.js
@@ -4,7 +4,7 @@ import axios from "axios";
 const isProduction = process.env.NODE_ENV === "production";
 
 // si la app esta ya en produccion colocara otra ruta de lo contrario usara localhost
-const URL =  isProduction ? "[Heroku Link goes here]" : "http://localhost:3000/api";
+const URL =  isProduction ? "https://iron-fitness.herokuapp.com/api" : "http://localhost:3000/api";
 axios.defaults.withCredentials = true;
 export const _axios = axios.create({
     baseURL: URL,
